299924b47985a4acf205e190563f47f61263c860
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-01-29 22:10:37 +0900

    sudoers.sh: supported macos as well

--- flutter.sh
+++ flutter.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Install flutter
 # ref. https://qiita.com/tomy0610/items/896dc8ec9ba95c33194f
+if [ "$(uname)" == 'Darwin' ]; then
+    if ! type gem >/dev/null 2>&1; then
+        echo "Run ruby.sh first to install gem"
+        exit 1
+    fi
+fi
 if ! type flutter >/dev/null 2>&1; then
     cd
     curl -L https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_1.22.6-stable.zip -o ~/flutter.zip
@@ -17,4 +23,10 @@ if ! type flutter >/dev/null 2>&1; then
     export PATH="$PATH:$DESTDIR/flutter/bin"
     rm ~/flutter.zip
 fi
+echo "Installing CocoaPods for Xcode ..."
+sudo gem install cocoapods
+pod setup
+
 flutter doctor
+sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
+sudo xcodebuild -runFirstLaunch
--- sudoers.sh
+++ sudoers.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 # Allow the user to run sudo without password
+FILENAME="/etc/sudoers.d/nirecom-users"
 if [ ! -f /etc/sudoers.d/nirecom-users ]; then
-    sudo sh -c 'echo "nire    ALL=NOPASSWD: ALL" >>/etc/sudoers.d/nirecom-users'
-    sudo chown root:root /etc/sudoers.d/nirecom-users
-    sudo chmod 440 /etc/sudoers.d/nirecom-users
+    sudo sh -c 'echo "nire    ALL=(ALL) NOPASSWD: ALL" >/etc/sudoers.d/nirecom-users'
+    sudo chmod 440 $FILENAME
+    if [ "$(uname)" == 'Darwin' ]; then
+        sudo chown root:wheel $FILENAME
+    elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+        sudo chown root:root $FILENAME
+    fi
 fi
