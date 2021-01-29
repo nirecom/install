fc365d5541b87daeeb4016478f468f36500f20db
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-01-29 20:48:39 +0900

    added flutter.sh

---
+++ flutter.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd
+curl -L https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_1.22.6-stable.zip -o ~/flutter.zip
+
+if ! type unzip /dev/null 2>&1; then
+    if [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+        sudo apt get unzip
+    fi
+fi
+unzip ~/flutter.zip
+export PATH="$PATH:$HOME/flutter/bin"
