a99c29a810f2babb21cb9a99bc8dcc4e7ee9608e
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-01-31 05:26:17 +0000

    bugfixes on zsh check

--- zsh.sh
+++ zsh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 [ ! -f /bin/zsh ] && sudo apt install -y zsh
-if [ $SHELL != "/bin/zsh" ]; then
+if [ ! "`echo $SHELL | grep zsh`" ]; then
     echo "Changing shell to zsh. Enter password ..."
     chsh -s $(which zsh)
 fi
