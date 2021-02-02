0a08e736092b4f81f7f7939f6e3744dc36b38fe4
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-02-02 21:28:34 +0900

    supported ubuntu scenario

--- source-highlight.sh
+++ source-highlight.sh
@@ -2,12 +2,13 @@
 echo "Installing source-highlight ..."
 if [ "$(uname)" == 'Darwin' ]; then
     brew install source-highlight
+    export LESSOPEN="| /usr/local/bin/src-hilite-lesspipe.sh %s"
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
     sudo apt install -y source-highlight
+    export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
 else
     echo "Not supported OS. Abort."
     exit 1
 fi
-export LESSOPEN="| /usr/local/bin/src-hilite-lesspipe.sh %s"
 export LESS='-R'
 exec $SHELL -l
