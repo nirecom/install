9e83d62fea41d4c3b68c3b4edc54631eda00d392
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-02-04 07:54:07 +0900

    install customized esc.style for source highlighting

--- source-highlight.sh
+++ source-highlight.sh
@@ -2,13 +2,23 @@
 echo "Installing source-highlight ..."
 if [ "$(uname)" == 'Darwin' ]; then
     brew install source-highlight
-    export LESSOPEN="| /usr/local/bin/src-hilite-lesspipe.sh %s"
+    export LDIR="/usr/local/bin"
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
     sudo apt install -y source-highlight
-    export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
+    export LDIR="/usr/share/source-highlight"
 else
     echo "Not supported OS. Abort."
     exit 1
 fi
+export LESSOPEN="| $LDIR/src-hilite-lesspipe.sh %s"
 export LESS='-R'
+
+echo "Customizing color table ..."
+DDIR=$HOME/dotfiles/source-highlight
+if [ -f $DDIR/esc.style ]; then
+    sudo mv $LDIR/esc.style $LDIR/esc.style.orig
+    sudo cp $DDIR/esc.style $LDIR
+    sudo chmod 644 $LDIR/esc.style
+    sudo chown root:root $LDIR/esc.style
+fi
 exec $SHELL -l
