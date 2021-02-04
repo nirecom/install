25502a3f52e63478cacfcc1beeec3776729e3588
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-02-04 19:08:35 +0900

    bugfix on macos

--- source-highlight.sh
+++ source-highlight.sh
@@ -2,15 +2,16 @@
 echo "Installing source-highlight ..."
 if [ "$(uname)" == 'Darwin' ]; then
     brew install source-highlight
-    export LDIR="/usr/local/bin"
-elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    export LDIR="/usr/local/share/source-highlight"
+    export LESSOPEN="| /usr/local/bin/src-hilite-lesspipe.sh %s"
+elif [[ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]]; then
     sudo apt install -y source-highlight
     export LDIR="/usr/share/source-highlight"
+    export LESSOPEN="| $LDIR/src-hilite-lesspipe.sh %s"
 else
     echo "Not supported OS. Abort."
     exit 1
 fi
-export LESSOPEN="| $LDIR/src-hilite-lesspipe.sh %s"
 export LESS='-R'
 
 echo "Customizing color table ..."
@@ -19,6 +20,8 @@ if [ -f $DDIR/esc.style ]; then
     sudo mv $LDIR/esc.style $LDIR/esc.style.orig
     sudo cp $DDIR/esc.style $LDIR
     sudo chmod 644 $LDIR/esc.style
-    sudo chown root:root $LDIR/esc.style
+    if [ ! "$(uname)" == 'Darwin' ] && [[ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]]; then
+        sudo chown root:root $LDIR/esc.style
+    fi
 fi
 exec $SHELL -l
