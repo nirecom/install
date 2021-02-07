5af705f716dd997e5cc40ce54c1862f0a66a319b
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-02-07 20:14:11 +0000

    bugfix

--- home-obsolete.sh
+++ home-obsolete.sh
@@ -3,6 +3,7 @@
 
 echo "Uninstall emacs: git cloning dir ..."
 GDIR=$HOME/.emacs.d/git
-if [ ! -d $GDIR ]; then
+if [ -d $GDIR ]; then
+    echo "Found. Removing ..."
     rm -r $HOME/.emacs.d/git
 fi
