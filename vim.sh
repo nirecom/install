65217010f76a3cd5688088d803b9f23afb75a3e3
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-02-08 02:13:50 +0000

    fixed git paths

--- vim.sh
+++ vim.sh
@@ -6,10 +6,10 @@ mkdir -p ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
 # plugins
-[ ! -d ./vim-sensible ] && git clone git://github.com/tpope/vim-sensible.git
+[ ! -d ./vim-sensible ] && git clone git@github.com:tpope/vim-sensible.git
 [ ! -d ./editorconfig-vim ] && git clone git@github.com:editorconfig/editorconfig-vim.git
-[ ! -d ./vim-colors-solarized ] && git clone git://github.com/altercation/vim-colors-solarized.git
+[ ! -d ./vim-colors-solarized ] && git clone git@github.com:altercation/vim-colors-solarized.git
 
 # vim-json
-[ ! -d ./vim-json ] && git clone https://github.com/elzr/vim-json.git
+[ ! -d ./vim-json ] && git clone git@github.com:elzr/vim-json.git
 ln -sf ~/dotfiles/filetype.vim ~/.vim/
