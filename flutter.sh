5d6725c55f4c34044d53a889ef975c7e79cfd4c8
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-02-12 06:31:58 +0900

    flutter.sh to use stable version at git

--- flutter.sh
+++ flutter.sh
@@ -7,33 +7,33 @@ if [ ! "$OSDIST" = "macos" ]; then
     echo "Not supported OS."
     exit 1
 fi
-if type gem >/dev/null 2>&1; then
-    echo "Run ruby.sh first to install gem"
+if ! type gem >/dev/null 2>&1; then
+    echo "Run ruby.sh first to install gem for CocoaPods"
     exit 1
 fi
 if ! type flutter >/dev/null 2>&1; then
     echo "Downloading flutter..."
     cd
-    curl -L https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_1.22.6-stable.zip -o ~/flutter.zip
-
-    if ! type unzip /dev/null 2>&1; then
-        brew install unzip
-    fi
-    DESTDIR="$HOME/development"
-    mkdir -p $DESTDIR
-    cd $DESTDIR
-    unzip ~/flutter.zip
-    export PATH="$PATH:$DESTDIR/flutter/bin"
-    rm ~/flutter.zip
+    git clone https://github.com/flutter/flutter.git -b stable
+    export PATH="$PATH:$HOME/flutter/bin"
+    #    curl -L https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_1.22.6-stable.zip -o ~/flutter.zip
+    #   mkdir -p $DESTDIR
+    #    cd $DESTDIR
+    #    unzip ~/flutter.zip
+    #DESTDIR="$HOME/development"
+    #export PATH="$PATH:$DESTDIR/flutter/bin"
+    #rm ~/flutter.zip
 fi
 echo "Installing CocoaPods for Xcode ..."
+# ref https://qiita.com/ShinokiRyosei/items/3090290cb72434852460
 sudo gem install cocoapods
 pod setup
 
-sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
-sudo xcodebuild -runFirstLaunch
+#sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
+#sudo xcodebuild -runFirstLaunch
 
+~/flutter/bin/flutter precache
 # Change channel, otherwise flutter doctor does not detect Android Studio plugin.
 # ref. https://stackoverflow.com/questions/52336654/android-studio-flutter-and-dart-plugins-not-recognized-by-flutter-doctor-but-p
-flutter channel beta
-flutter doctor
+#flutter channel beta
+~/flutter/bin/flutter doctor
