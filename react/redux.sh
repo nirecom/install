ae2faace21334676c1bc50790ddbc0a4257cb880
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-02-22 21:54:55 +0900

    added eslint init

--- react/redux.sh
+++ react/redux.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-npm install --save eslint
+#npm install --save eslint
+npm install --save-dev eslint
+npx eslint --init
 npm install --save redux
 npm install --save react-redux
 npm install --save-dev --legacy-peer-deps redux-devtools
