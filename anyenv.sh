91ba28e8ca3711b99da02b915a3c1d13fda10103
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-02-28 01:00:35 +0000

    updating anyenv.sh

--- anyenv.sh
+++ anyenv.sh
@@ -12,6 +12,19 @@ case "$OSDIST" in
             git clone https://github.com/anyenv/anyenv ~/.anyenv
         fi
         ;;
+    "amazon" )
+        cd /usr/local
+        sudo git clone https://github.com/anyenv/anyenv
+        sudo chgrp -R wheel anyenv
+        sudo chmod -R g+rwxXs anyenv
+        cd /etc/profile.d/
+        cat >anyenv.sh
+export ANYENV_ROOT="/usr/local/anyenv"
+export ANYENV_DEFINITION_ROOT="/usr/local/anyenv/share/anyenv-install"
+export PATH="${ANYENV_ROOT}/bin:${PATH}"
+eval "$(anyenv init --no-rehash -)"
+EOF
+        ;;
     * )
         echo "Not supported OS."
         exit 1
