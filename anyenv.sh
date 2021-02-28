cd7da8e39f1ddd7c5c4c0b64358e1d3037d45a81
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-02-28 01:13:01 +0000

    anyenv.sh

--- anyenv.sh
+++ anyenv.sh
@@ -2,6 +2,12 @@
 # Install anyenv
 source ./bin/detectos.sh
 
+if type anyenv >/dev/null 2>&1; then
+    echo "anyenv is already installed. Skip."
+    exit 1
+fi
+echo "Installing anyenv..."
+
 case "$OSDIST" in
     "macos" )
         brew install anyenv
@@ -13,17 +19,20 @@ case "$OSDIST" in
         fi
         ;;
     "amazon" )
+        # ref https://caramelcase.com/aws-anyanv-rubyenv-nodenv/#toc4
         cd /usr/local
         sudo git clone https://github.com/anyenv/anyenv
         sudo chgrp -R wheel anyenv
         sudo chmod -R g+rwxXs anyenv
         cd /etc/profile.d/
-        cat >anyenv.sh
+        cat <<EOF >anyenv.sh
 export ANYENV_ROOT="/usr/local/anyenv"
 export ANYENV_DEFINITION_ROOT="/usr/local/anyenv/share/anyenv-install"
 export PATH="${ANYENV_ROOT}/bin:${PATH}"
 eval "$(anyenv init --no-rehash -)"
 EOF
+        anyenv install --init
+        exec $SHELL -l
         ;;
     * )
         echo "Not supported OS."
