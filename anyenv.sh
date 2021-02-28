234205bb5433b28788bc34b959cade6aace1d765
Hideaki Nire (nirecom) <hideaki@nire.com>
2021-02-28 06:27:49 +0000

    anyenv done

--- anyenv.sh
+++ anyenv.sh
@@ -12,11 +12,13 @@ case "$OSDIST" in
     "macos" )
         brew install anyenv
         eval "$(anyenv init -)"
+        ~/.anyenv/bin/anyenv install --init
         ;;
     "ubuntu" )
         if [ ! -d ~/.anyenv ]; then
             git clone https://github.com/anyenv/anyenv ~/.anyenv
         fi
+        ~/.anyenv/bin/anyenv install --init
         ;;
     "amazon" )
         # ref https://caramelcase.com/aws-anyanv-rubyenv-nodenv/#toc4
@@ -27,20 +29,14 @@ case "$OSDIST" in
         sudo chgrp -R wheel anyenv
         sudo chmod -R g+rwxXs anyenv
         cd /etc/profile.d/
-        if [ ! -f /etc/profile.d/anyenv.sh ]; then
-            echo "Installing /etc/profile.d/anyenv.sh..."
-            sudo sh -c 'cat <<EOF >anyenv.sh
-export ANYENV_ROOT="/usr/local/anyenv"
-export ANYENV_DEFINITION_ROOT="/usr/local/anyenv/share/anyenv-install"
-export PATH="${ANYENV_ROOT}/bin:${PATH}"
-eval "$(anyenv init --no-rehash -)"
-EOF'
+        if [ ! -f anyenv.sh ]; then
+            sudo cp -p ~/install/anyenv-profile.sh anyenv.sh
+            sudo chown root.wheel anyenv.sh
         fi
-        anyenv install --init
+        /usr/local/anyenv/bin/anyenv install --init
         exec $SHELL -l
         ;;
     * )
         echo "Not supported OS."
         exit 1
 esac
-~/.anyenv/bin/anyenv install --init
