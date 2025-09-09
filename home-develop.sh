84d2924994c9d7549e7701db60d0ad806fa6ac0f
Hideaki Nire (nirecom) <hideaki@nire.com>
2025-09-09 21:09:23 +0900

    disabled ruby installation

--- home-develop.sh
+++ home-develop.sh
@@ -5,7 +5,7 @@
 ./anyenv.sh
 ./nodenv-node.sh
 #./kotlin.sh
-./rbenv-ruby.sh
+#./rbenv-ruby.sh
 
 echo "Run flutter.sh later, since it requires installation of Android Studio and Android SDK command line tools."
 #./flutter.sh
